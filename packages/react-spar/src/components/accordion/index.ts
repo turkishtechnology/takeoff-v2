9ea12e15157b5b7c32f11dd35d4251d34e49c80b
harundemr <harundemirp@gmail.com>
2026-04-18 23:24:21 +0300

    refactor: remove skills and current components

--- packages/react-spar/src/components/accordion/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Accordion';
-export * from './types';
-export { accordionClassNames, accordionItemClassNames } from './AccordionBase';
--- packages/react-spar/src/components/checkbox/index.ts
+++
@@ -1,3 +0,0 @@
-export { Checkbox } from './Checkbox';
-export * from './types';
-export { checkboxClassNames } from './CheckboxBase';
